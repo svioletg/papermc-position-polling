f1a8c094aa26d26f171bd449f08622549a3104f0
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-07-15 16:36:21 -0500

    init commit

---
+++ src/main/kotlin/io/github/svioletg/positionPolling/PositionPolling.kt
@@ -0,0 +1,14 @@
+package io.github.svioletg.positionPolling
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PositionPolling : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
