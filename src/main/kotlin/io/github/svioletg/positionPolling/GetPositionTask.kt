2b2a25505e2a4c339b94a5b64a8c1ea580ea9c49
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-07-15 23:27:31 -0500

    remove polling broadcast message

--- src/main/kotlin/io/github/svioletg/positionPolling/GetPositionTask.kt
+++ src/main/kotlin/io/github/svioletg/positionPolling/GetPositionTask.kt
@@ -9,13 +9,6 @@ class GetPositionTask(private val plugin: PositionPolling) : Runnable {
     override fun run() {
         if (!this.plugin.doPolling) return
 
-        val time = System.currentTimeMillis()
         this.plugin.db.recordPlayerPos(this.plugin.server.onlinePlayers.toTypedArray())
-        for (player in this.plugin.server.onlinePlayers) {
-            val name: String = player.name
-            val uuid: UUID = player.uniqueId
-            val pos: Location = player.location
-            this.plugin.server.broadcast(Component.text("(${time}) $name ($uuid) at: ${pos.x} ${pos.y} ${pos.z}"))
-        }
     }
 }
