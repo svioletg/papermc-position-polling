16cd8cff28689857beee438ef484b7003233d4f6
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-07-15 23:24:12 -0500

    remove unused command PluginCommands.helloCommand()

--- src/main/kotlin/io/github/svioletg/positionPolling/PluginCommands.kt
+++ src/main/kotlin/io/github/svioletg/positionPolling/PluginCommands.kt
@@ -7,14 +7,6 @@ import io.papermc.paper.command.brigadier.Commands
 import net.kyori.adventure.text.Component
 
 class PluginCommands(private val plugin: PositionPolling) {
-    fun helloCommand(): LiteralArgumentBuilder<CommandSourceStack?>? {
-        return Commands.literal("hello").executes(fun(ctx): Int {
-            this.plugin.server.broadcast(Component.text("Hello!"))
-
-            return Command.SINGLE_SUCCESS
-        })
-    }
-
     fun turnPollingOn(): LiteralArgumentBuilder<CommandSourceStack?>? {
         return Commands.literal("on").executes(fun(ctx): Int {
             this.plugin.doPolling = true
