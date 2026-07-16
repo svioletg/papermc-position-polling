533e2410790fb1976f72cd4167d395e5ea064524
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-07-16 12:24:43 -0500

    remove unused imports from GetPositionTask

--- src/main/kotlin/io/github/svioletg/positionPolling/GetPositionTask.kt
+++ src/main/kotlin/io/github/svioletg/positionPolling/GetPositionTask.kt
@@ -1,10 +1,5 @@
 package io.github.svioletg.positionPolling
 
-import net.kyori.adventure.text.Component
-import org.bukkit.Location
-import java.time.LocalTime
-import java.util.UUID
-
 class GetPositionTask(private val plugin: PositionPolling) : Runnable {
     override fun run() {
         if (!this.plugin.doPolling) return
